732836ae4168f64788180e9708dbbd1b220b56c2
Tigr046 <tigranadtbekyan@yandex.ru>
2019-12-26 00:11:31 +0300

    ghc]

---
+++ Shop/Controllers/CartController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shop.DBContext;
+using Shop.Helper;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    public class CartController : Controller
+    {
+        private ApplicationContext db = new ApplicationContext();
+
+        public IActionResult Index()
+        {
+            var cart = SessionHelper.GetObjectFromJson<List<PhoneItem>>(HttpContext.Session, "cart");
+            ViewBag.cart = cart;
+            return View();
+        }
+
+        public IActionResult Buy(Guid id)
+        {
+            if (SessionHelper.GetObjectFromJson<List<PhoneItem>>(HttpContext.Session, "cart") == null)
+            {
+                var cart = new List<PhoneItem>();
+                cart.Add(new PhoneItem()
+                {
+                    Phone = db.Phones.Find(id),
+                    Quantity = 1
+                });
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
+            }
+            else
+            {
+                var cart = SessionHelper.GetObjectFromJson<List<PhoneItem>>(HttpContext.Session, "cart");
+                int index = Exists(cart, id);
+                if (index == -1)
+                {
+                    cart.Add(new PhoneItem()
+                    {
+                        Phone = db.Phones.Find(id),
+                        Quantity = 1
+                    });
+                }
+                else
+                {
+                    cart[index].Quantity++;
+                }
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
+
+            }
+            return RedirectToAction("Index");
+        }
+        private int Exists(List<PhoneItem> cart, Guid id)
+        {
+            for (int i = 0; i < cart.Count; i++)
+            {
+                if (cart[i].Phone.Id == id)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Shop/DBContext/AppliactionContext.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Shop.Models;
+
+namespace Shop.DBContext
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder();
+            var configuration = builder.Build();
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Poster;Username=postgres;Password=zxcasdqwe");
+        }
+    }
+}
